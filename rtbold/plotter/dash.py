a03c68a1b1193c7e1257f6bc40d8e4b2ea69d06f
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2024-05-13 14:25:55 -0400

    put the oop back into dash plotter

--- rtbold/plotter/dash.py
+++ rtbold/plotter/dash.py
@@ -8,61 +8,68 @@ import random
 logger = logging.getLogger('ui')
 
 class DashPlotter:
-    _instances = list()
-
     def __init__(self):
         self._app = Dash('Realtime fMRI is fun')
         self._title = 'Realtime fMRI Motion'
-        self.setup()
+        self._instances = list()
+        self.init_page()
+        self.init_callbacks()
 
-    def setup(self):
+    def init_page(self):
         self._app.layout = html.Div([
             html.H2(children=self._title, style={'textAlign':'center'}),
             html.Div(id='live-update-text'),
-            dcc.Graph(id='live-update-translations'),
             dcc.Graph(id='live-update-rotations'),
+            dcc.Graph(id='live-update-displacements'),
             dcc.Interval(
                 id='interval-component',
-                interval=2*1000, # in milliseconds
+                interval=1*1000,
                 n_intervals=0
             )
         ])
 
-    @callback(
-        Output('live-update-translations', 'figure'),
-        Input('interval-component', 'n_intervals')
-    )
-    def update_graph(n):
-        df = DashPlotter.df()
-        line = px.line(df, x='N', y=['superior', 'left', 'posterior'])
-        line.update_layout(
-            title='Displacements',
+    def init_callbacks(self):
+        self._app.callback(
+            Output('live-update-rotations', 'figure'),
+            Input('interval-component', 'n_intervals'),
+        )(self.update_rotations)
+
+        self._app.callback(
+            Output('live-update-displacements', 'figure'),
+            Input('interval-component', 'n_intervals'),
+        )(self.update_displacements)
+
+    def update_displacements(self, n):
+        df = self.df()
+        fig = px.line(df, x='N', y=['superior', 'left', 'posterior'])
+        fig.update_layout(
             yaxis_title='mm',
             legend={
                 'title': ''
+            },
+            title={
+                'text': 'Displacements'
             }
         )
-        return line 
+        return fig
 
-    @callback(
-        Output('live-update-rotations', 'figure'),
-        Input('interval-component', 'n_intervals')
-    )
-    def update_graph(n):
-        df = DashPlotter.df()
+    def update_rotations(self, n):
+        df = self.df()
         line = px.line(df, x='N', y=['roll', 'pitch', 'yaw'])
         line.update_layout(
-            title='Rotations',
             yaxis_title='degrees (ccw)',
             legend={
                 'title': ''
+            },
+            title={
+                'text': 'Rotations'
             }
         )
         return line 
 
-    def df():
+    def df(self):
         arr = list()
-        for i,instance in enumerate(DashPlotter._instances, start=1):
+        for i,instance in enumerate(self._instances, start=1):
             volreg = instance['volreg']
             if volreg:
                 arr.append([i] + volreg)
@@ -73,4 +80,4 @@ class DashPlotter:
         self._app.run(debug=True)
 
     def listener(self, instances):
-        DashPlotter._instances = instances
+        self._instances = instances
