1f11c01df5620f1c69fc2abae7d6098f09652011
Daniel Asay <danielasay@fas.harvard.edu>
2024-05-20 16:48:31 -0400

    add setup.py and __version__.py files

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'pyaml',
+    'pygame',
+    'plotext',
+    'textual',
+    'textual-dev',
+    'pynetdicom',
+    'nibabel',
+    'matplotlib'
+]
+
+about = dict()
+with open(os.path.join(here, 'rtbold', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=find_packages(),
+    package_data={
+        '': ['*.yaml', '*.tcss', '*.mp3']
+    },
+    include_package_data=True,
+    scripts=[
+        'scripts/start.py'
+    ],
+    install_requires=requires
+)
