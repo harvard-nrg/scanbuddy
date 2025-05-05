aa09e3df3744c9ced0cb3ce7734d83094f98a018
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2025-05-05 10:35:03 -0400

    use config file to pull message broker host and port

--- scanbuddy/broker/redis.py
+++ scanbuddy/broker/redis.py
@@ -6,9 +6,10 @@ import redis.exceptions
 logger = logging.getLogger(__name__)
 
 class MessageBroker:
-    def __init__(self, host='localhost', port=6379):
-        self._host = host
-        self._port = port
+    def __init__(self, config, host='localhost', port=6379):
+        self._config = config
+        self._host = self._config.find_one('$.broker.host', default=host)
+        self._port = self._config.find_one('$.broker.port', default=port)
         self._conn = None
         self._uri = f'redis://{self._host}:{self._port}'
         self.connect()
