c909c7f1a3688ee2d5730f6a165e3744a965353e
Daniel Asay <danielasay@fas.harvard.edu>
2025-11-19 14:01:43 -0500

    bug fix for multi-echo data. scanbuddy will now read the file name from dcm2niix output

--- scanbuddy/proc/converter.py
+++ scanbuddy/proc/converter.py
@@ -1,4 +1,5 @@
 import os
+import re
 import glob
 import json
 import time
@@ -40,16 +41,24 @@ class Converter:
         cmdstr = json.dumps(dcm2niix_cmd, indent=2)
         logger.debug(f'running dcm2niix: {cmdstr}')
 
-        output = subprocess.check_output(dcm2niix_cmd, stderr=subprocess.STDOUT)
+        output = subprocess.check_output(dcm2niix_cmd, stderr=subprocess.STDOUT, text=True)
 
         logger.debug(f'dcm2niix output: {output}')
 
-        self._nii_file = os.path.join(out_dir, f'{modality}_{num}.nii')
+        self._nii_file = self.get_nii_file(output)
+
+        if not self._nii_file:
+            logger.critical(f'could not find converted nifti file for {dicom}')
 
     def insert_array(self, instance):
         instance['nii_path'] = self._nii_file
 
-
-
-
+    def get_nii_file(self, output):
+        for line in output.splitlines():
+            match = re.search(r'Convert 1 DICOM as (/\S+)', line)
+            if match:
+                if os.path.isfile(str(match.group(1)) + '.nii'):
+                    return str(match.group(1)) + '.nii'
+                elif os.path.isfile(str(match.group(1)) + '.nii.gz'):
+                    str(match.group(1)) + '.nii.gz'
 
